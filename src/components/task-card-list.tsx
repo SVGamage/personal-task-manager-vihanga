d87703ced53e868d0a8663af5efbdfd365510a47
SVGamage <119811762+SVGamage@users.noreply.github.com>
2025-03-21 20:13:24 +0530

    feat: task not found UI

--- src/components/task-card-list.tsx
+++ src/components/task-card-list.tsx
@@ -1,5 +1,6 @@
 import { TaskWithCategory } from "@/app/types";
 import TaskCard from "./task-card";
+import ItemNotFound from "./item-not-found";
 
 interface TaskCardListProps {
   tasks: TaskWithCategory[];
@@ -7,9 +8,13 @@ interface TaskCardListProps {
 export default async function TaskCardList({ tasks }: TaskCardListProps) {
   return (
     <div className="grid gap-4">
-      {tasks.map((task) => {
-        return <TaskCard key={task.id} task={task} />;
-      })}
+      {tasks.length > 0 ? (
+        tasks.map((task) => {
+          return <TaskCard key={task.id} task={task} />;
+        })
+      ) : (
+        <ItemNotFound name="Tasks" />
+      )}
     </div>
   );
 }
